13f4f2ee778feeaf45ecc279f5b2c9f78521b35c
BuildTools <unconfigured@null.spigotmc.org>
2020-03-10 23:44:44 +0100

    Login Page

--- src/loaders/login.php
+++ src/loaders/login.php
@@ -1,18 +1,5 @@
 <?php
-    //session_start();
-    //if ($_SESSION["login_fail"]) {
-    //    $login_fail =  $_SESSION["login_fail"];
-    //    if ($login_fail) {
-    //        $message = "Invalid login or password";
-    //        $_SESSION["message"] = $message;
-    //    }
-    //}
-    //else {
-    //    $_SESSION["login_fail"] = false;
-    //    $message = '';
-    //    $_SESSION["message"] = $message;
-    //}
-    $title = 'Login';
+    $title = 'login';
     $content = '../views/login.php';
-    include('../layouts/layout-public.php');
+    include("../layouts/layout-login.php");
 ?>
